ef0e72bd5060da55224bb17babbab233892568d9
ctlemos <catarinatlemost@gmail.com>
2024-12-03 15:41:34 +0000

    hamburger menu criado

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const navbarHamburger = document.getElementById('navHamburger');
-    const navbarMenu = document.getElementById('mainNav');
-    const menuItems = document.getElementById('menu-items');
+  const navbarHamburger = document.getElementById('navHamburger');
+  const navbarMenu = document.getElementById('mainNav');
+  const menuItems = document.getElementById('menu-items');
 
-    // Toggle menu on hamburger click
-    navbarHamburger.addEventListener('click', () => {
-      navbarMenu.classList.toggle('active');
-      navbarHamburger.classList.toggle('active');
-    });
+  // Toggle menu on hamburger click
+  navbarHamburger.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
+    navbarHamburger.classList.toggle('active');
+  });
 
-    // Close menu when a menu item is clicked
-    menuItems.addEventListener('click', () => {
-      navbarMenu.classList.remove('active');
-      navbarHamburger.classList.remove('active');
-    });
+  // Close menu when a menu item is clicked
+  menuItems.addEventListener('click', () => {
+    navbarMenu.classList.remove('active');
+    navbarHamburger.classList.remove('active');
+  });
 });
